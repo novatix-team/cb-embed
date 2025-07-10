24efe9e02b894d41bb27363785dc2b690b103c1e
Nikitas <nikitas@novatix.ch>
2025-07-10 13:27:40 +0200

    buttons hover

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -111,7 +111,7 @@ export const Bubble = (props: BubbleProps) => {
               {/* Cross button For only mobile screen use this <Show when={isBotOpened() && window.innerWidth <= 640}>  */}
               <button
                 onClick={closeBot}
-                class="py-2 pr-3 absolute top-0 right-[-8px] m-[6px] bg-transparent text-white rounded-full z-50 disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100 transition-all filter hover:brightness-90 active:brightness-75"
+                class="py-2 pr-3 absolute top-0 right-[-8px] m-[6px] bg-transparent text-white rounded-full z-50 disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100 transition-all filter hover:brightness-90 active:brightness-75 hover:scale-125"
                 title="Close Chat"
               >
                 <svg viewBox="0 0 24 24" width="24" height="24">
