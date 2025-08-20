ce6e345898da7c9250a46efce16e0f8e9e010399
Nadia <nadia@novatix.ch>
2025-08-20 12:31:41 +0200

    fifth attempt : fix disclaimer text wrapping on mobile

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1842,7 +1842,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
             </div>
             <div
               ref={chatContainer}
-              class="overflow-y-scroll flex flex-col flex-grow min-w-full w-full px-3 pt-[90px] mt-[30px] sm:mt-[30px] md:mt-[35px] relative scrollable-container chatbot-chat-view scroll-smooth"
+              class="overflow-y-scroll flex flex-col flex-grow min-w-full w-full px-3 pt-[90px] mt-[50px] sm:mt-[50px] md:mt-[70px] relative scrollable-container chatbot-chat-view scroll-smooth"
             >
               <For each={[...messages()]}>
                 {(message, index) => {
